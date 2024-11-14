6755cb85d694732d24596213fecce323636f937e
Ihor Radchenko <yantar92@posteo.net>
2024-11-14 15:34:45 +0100

    fix

--- src/IMDgroup/gorun/gorun.py
+++ src/IMDgroup/gorun/gorun.py
@@ -105,7 +105,7 @@ def backup_current_dir(to: str) -> None:
     barf_if_no_cmd('rsync')
     print(f"Backing up {os.getcwd()} ...")
     os.mkdir(to)
-    subprocess.check_call(f'rsync * {to}')
+    subprocess.check_call(f'rsync * {to}', shell=True)
     print(f"Backing up {os.getcwd()} ... done")
 
 
