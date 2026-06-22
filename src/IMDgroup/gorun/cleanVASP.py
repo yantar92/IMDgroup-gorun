81f0be703cd4ed0fb4eccb26c71b67c21e56ca95
Ihor Radchenko <yantar92@posteo.net>
2026-06-22 08:36:56 +0200

    generate_potcar: Fix ASE recommended setup not being up to date for W

--- src/IMDgroup/gorun/cleanVASP.py
+++ src/IMDgroup/gorun/cleanVASP.py
@@ -192,7 +192,14 @@ def generate_potcar(path='.', keep_existing=False) -> None:
             print(f'{path}: Not updating existing POTCAR.')
             return
         atoms = ase.io.vasp.read_vasp(poscar_path)
-        calc_temp = Vasp(xc='PBE', setups={'base': 'recommended'})
+        calc_temp = Vasp(
+            xc='PBE',
+            setups={
+            'base': 'recommended',
+            # ASE bug - dictionary not updated and
+            # W_sv is recommended on
+            # https://vasp.at/wiki/index.php/Available_pseudopotentials
+            'W': 'W_sv'})
         calc_temp.initialize(atoms)
         size_before = 0
         prev_potcar = None
